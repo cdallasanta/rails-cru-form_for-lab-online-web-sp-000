dcaa637a7fc626fdcaca9adad1b468f0f7e5d7ff
Chris Dalla Santa <chris.dallasanta@gmail.com>
2019-05-13 20:21:24 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,2 +1,22 @@
 class SongsController < ApplicationController
+  def new
+    @song = Song.new
+  end
+
+  def create
+    song = Song.create(song_params)
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  private
+
+  def song_params
 end
