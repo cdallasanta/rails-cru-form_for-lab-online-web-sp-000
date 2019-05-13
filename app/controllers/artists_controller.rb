17e541eaba8eb6d96c25fc6b5ea4fae646364cd0
Chris Dalla Santa <chris.dallasanta@gmail.com>
2019-05-13 20:27:41 +0000

    Automatically backed up by Learn

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,2 +1,31 @@
 class ArtistsController < ApplicationController
+  def new
+    @artist = Artist.new
+  end
+
+  def create
+    artist = Artist.create(artist_params)
+
+    redirect_to artist_path(artist)
+  end
+
+  def show
+    @artist = Artist.find(params[:id])
+  end
+
+  def edit
+  end
+
+  def update
+    artist = Artist.find(params[:id])
+    artist.update(artist_params)
+
+    redirect_to artist_path(artist)
+  end
+
+  private
+
+  def artist_params
+    params.require(:artist).permit(:name)
+  end
 end
